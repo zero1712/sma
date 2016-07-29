a3bec3cb10527e401d1722e0e3e2864cb9b36066
zero1808 <zero.leo1894@gmail.com>
2016-07-29 02:06:08 -0500

    cambios terminado de agenda vista y model

--- application/views/agenda/agenda.php
+++ application/views/agenda/agenda.php
@@ -48,7 +48,17 @@
 <script src="<?php echo base_url();?>js/plugins/clockpicker/clockpicker.js"></script>
 <script src="<?php echo base_url();?>js/plugins/cropper/cropper.min.js"></script>
 
-
+ <!-- -------------------------- chat  ------------------------------ -->
+ 
+ <!--Start of Zopim Live Chat Script-->
+<script type="text/javascript">
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+$.src="//v2.zopim.com/?44cPfbnM4alkYMHZG3eSs3f4g7L19VDI";z.t=+new Date;$.
+type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+</script>
+<!--End of Zopim Live Chat Script-->
 <!-- ------------------------------------Validaciones------------------------------- -->       
 <script>
 $(document).ready(function(){
