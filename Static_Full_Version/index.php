bf0719abb345d6d47ea494f6ae5f7f0c287136d4
Harvard2205 <manjigm@gmail.com>
2016-08-03 20:57:57 -0500

    cambios en menu

--- Static_Full_Version/index.php
+++ Static_Full_Version/index.php
@@ -24,10 +24,50 @@
 <body>
 
     <div id="wrapper">
-
-    <?php 
-	     include ("menu.php")
-    ?>
+<!-- menu -->
+<nav class="navbar-default navbar-static-side" role="navigation">
+        <div class="sidebar-collapse">
+            <ul class="nav metismenu" id="side-menu">
+                <li class="nav-header">
+                    <div class="dropdown profile-element"> <span>
+                            <img alt="image" class="img-circle" src="img/profile_small.jpg" />
+                             </span>
+                        <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                            <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">Eruviel Ávila</strong>
+                             </span> <span class="text-muted text-xs block">Gobernador Edomex <b class="caret"></b></span> </span> </a>
+                        <ul class="dropdown-menu animated fadeInRight m-t-xs">
+                            <li><a href="profile.html">Perfil</a></li>
+                            <li><a href="contacts.html">Contactos</a></li>
+                            <li><a href="mailbox.html">Bandeja de Entrada</a></li>
+                            <li class="divider"></li>
+                            <li><a href="login.html">Logout</a></li>
+                        </ul>
+                    </div>
+                    <div class="logo-element">
+                        IN+
+                    </div>
+                </li>
+                <li>
+                    <a href="#"><i class="fa fa-flask"></i> <span class="nav-label">Panel de Control</span></a>
+                </li>
+                <li>
+                <li>
+                    <a href="timeline.html"><i class="fa fa-diamond"></i> <span class="nav-label">Audiencia</span></a>
+                </li>
+                <li>
+                    <a href="calendar.html"><i class="fa fa-desktop"></i> <span class="nav-label">Agenda</span></a>
+                </li>
+				<li>
+                    <a href="ecommerce_product_list.html"><i class="fa fa-edit"></i> <span class="nav-label">Documentos</span></a>
+                </li>
+				<li>
+                    <a href="eventos.html"><i class="fa fa-database"></i> <span class="nav-label">Evento</span></a>
+                </li>
+            </ul>
+                </li>
+        </div>
+    </nav>
+<!-- menu -->
         <div id="page-wrapper" class="gray-bg">
         <div class="row border-bottom">
         <nav class="navbar navbar-static-top" role="navigation" style="margin-bottom: 0">
